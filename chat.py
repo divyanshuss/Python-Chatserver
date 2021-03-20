628f9f556962980ae6419736777a28a1540ec55c
divyanshuss <71714243+divyanshuss@users.noreply.github.com>
2021-03-20 12:33:38 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -29,14 +29,10 @@ def sender(ip2, port2):
         print('Your Message : ', msg)
         
 
-sys.stdout.write("Reciever's IP address: ")
-sys.stdout.flush()
-ip1=sys.stdin.readline()
-p1 = int(input("Reciever's port number : "))
 
-sys.stdout.write("Sender's IP address: ")
-sys.stdout.flush()
-ip2=sys.stdin.readline()
+ip1= input("Enter receiver's ip address")
+p1 = int(input("Reciever's port number : ")
+ip2= input("Enter senders's ip address")
 p2 = int(input(" Sender's port number : "))
 
 t1 = Thread(target=sender, args=(ip2, p2))
